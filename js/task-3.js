ca57121c06f02bb805a67a45c7155cfef5f8f0a3
Berkay Torunlar <berkaytorunlar@Berkay-MacBook-Air.local>
2025-05-15 01:55:32 +0300

    js-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-getElementWidth  function(content, padding, border){
+function getElementWidth(content, padding, border){
     
     return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
 }
